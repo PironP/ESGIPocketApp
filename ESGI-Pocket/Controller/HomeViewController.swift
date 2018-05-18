bae5c00a9efec0b9c2de433dca55d271b592c71b
PironP <piron.pierre@gmail.com>
2018-05-18 09:58:50 +0200

    Bind homeview's button to one func

--- ESGI-Pocket/Controller/HomeViewController.swift
+++ ESGI-Pocket/Controller/HomeViewController.swift
@@ -23,29 +23,26 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func lessonsButtonPressed(_ sender: Any) {
-        let lessonsListView = LessonsListViewController()
-        self.present(lessonsListView, animated: true, completion: nil)
-    }
-    
-    @IBAction func discussionsButtonPressed(_ sender: Any) {
-        let discussionsListView = DiscussionsListViewController()
-        self.present(discussionsListView, animated: true, completion: nil)
-    }
-    
-    @IBAction func messagesButtonPressed(_ sender: Any) {
-        let messagesListView = MessagesListViewController()
-        self.present(messagesListView, animated: true, completion: nil)
-    }
-    
-    @IBAction func quizButtonPressed(_ sender: Any) {
-        let quizListView = QuizListViewController()
-        self.present(quizListView, animated: true, completion: nil)
-    }
-    
-    @IBAction func planningButtonPressed(_ sender: Any) {
-        let planningView = PlanningViewController()
-        self.present(planningView, animated: true, completion: nil)
+    @IBAction func navigationButtonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            let lessonsListView = LessonsListViewController()
+            self.present(lessonsListView, animated: true, completion: nil)
+        case 1:
+            let discussionsListView = DiscussionsListViewController()
+            self.present(discussionsListView, animated: true, completion: nil)
+        case 2:
+            let messagesListView = MessagesListViewController()
+            self.present(messagesListView, animated: true, completion: nil)
+        case 3:
+            let quizListView = QuizListViewController()
+            self.present(quizListView, animated: true, completion: nil)
+        case 4:
+            let planningView = PlanningViewController()
+            self.present(planningView, animated: true, completion: nil)
+        default:
+            return
+        }
     }
     
     @IBAction func disconnectButtonPressed(_ sender: Any) {
---
+++ ESGI-Pocket/Controller/MessageViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MessageViewCell.swift
+//  ESGI-Pocket
+//
+//  Created by pierre piron on 17/05/2018.
+//  Copyright © 2018 pierre piron. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewCell: UITableViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
