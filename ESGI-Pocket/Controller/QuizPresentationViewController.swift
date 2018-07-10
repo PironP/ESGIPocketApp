84fc4f16c6bc046df1fcc6c9cba26ad40c43ff17
Unknown <piron.pierre@gmail.com>
2018-07-10 18:23:45 +0200

    Create view for quiz

---
+++ ESGI-Pocket/Controller/QuizPresentationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuizPresentationViewController.swift
+//  ESGI-Pocket
+//
+//  Created by pierre piron on 09/07/2018.
+//  Copyright © 2018 pierre piron. All rights reserved.
+//
+
+import UIKit
+
+class QuizPresentationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
